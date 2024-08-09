5bbe0d3e5d7c890dc3473dfde296d57db8302163
root <root@HSC-SFTWARE13.hsc.local>
2024-08-09 09:46:42 -0400

    init

---
+++ lua/plugins/cat.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+    }
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    }
+  },
+}
