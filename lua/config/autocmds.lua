6fa64ca0ff52e416a958d511b96f8be8dbc09251
Adam Dew <adew@HSC-SFTWARE13.hsc.local>
2024-08-14 16:49:43 -0400

    Added some LSP language specific stuff

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,4 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-vim.cmd[[highlight Visual guifg=white guibg=green]]
+vim.cmd([[highlight Visual guifg=white guibg=green]])
+vim.cmd([[highlight Cursor guifg=black guibg=green]])
+
+require("lspconfig").intelephense.setup({
+  settings = {
+    intelephense = {
+      format = { braces = "k&r" },
+      environment = { phpVersion = "8.1.0" },
+    },
+  },
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,11 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.o.tabstop = 4        -- A TAB character looks like 4 spaces
-vim.o.expandtab = true   -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 4    -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4     -- Number of spaces inserted when indenting- Add any additional options here
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting- Add any additional options here
 vim.g.autoformat = false -- Disable autoformating
 vim.g.nofixeol = true -- Disable adding newlines at the end of files
 vim.g.nofixendofline = true
-vim.keymap.set('n', '<C-c>', '<Esc>')
+vim.keymap.set("n", "<C-c>", "<Esc>")
