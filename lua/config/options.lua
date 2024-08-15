08f9e4303514c96445be01f3afa3d4ed5d72fa64
Adam Dew <adew@HSC-SFTWARE13.hsc.local>
2024-08-15 15:53:15 -0400

    More qol settings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,5 +8,15 @@ vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting- Add any additi
 vim.g.autoformat = false -- Disable autoformating
 vim.g.nofixeol = true -- Disable adding newlines at the end of files
 vim.g.nofixendofline = true
-vim.keymap.set("n", "<C-c>", "<Esc>")
+vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.opt.cursorlineopt='number'
+
+--Centered half page up and down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Moving highlighted sections easily
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
