dfc408c6421dc4a1731af128bbc986c2f9f45222
Adam Dew <adew@HSC-SFTWARE13.hsc.local>
2024-08-15 15:54:44 -0400

    formatting

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,14 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
-vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting- Add any additional options here
-vim.g.autoformat = false -- Disable autoformating
-vim.g.nofixeol = true -- Disable adding newlines at the end of files
+
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.g.autoformat = false
+vim.g.nofixeol = true
 vim.g.nofixendofline = true
-vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.opt.cursorlineopt='number'
 
 --Centered half page up and down
@@ -20,3 +20,5 @@ vim.keymap.set("n", "N", "Nzzzv")
 -- Moving highlighted sections easily
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("i", "<C-c>", "<Esc>")
