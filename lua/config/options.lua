1b759c18ef3308046e47e34a8194db0d86804bc6
Adam Dew <adew@HSC-SFTWARE13.hsc.local>
2024-08-16 12:20:16 -0400

    moved stuff to the right place

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,15 +10,3 @@ vim.g.autoformat = false
 vim.g.nofixeol = true
 vim.g.nofixendofline = true
 vim.opt.cursorlineopt='number'
-
---Centered half page up and down
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-
--- Moving highlighted sections easily
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-vim.keymap.set("i", "<C-c>", "<Esc>")
