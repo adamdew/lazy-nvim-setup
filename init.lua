b19eeb4b7191054249e7a8fd08ca424868575e72
adamdew <adamdew20@gmail.com>
2024-08-17 11:55:12 -0400

    blade ts support

--- init.lua
+++ init.lua
@@ -1,2 +1,24 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("conform").setup({
+    formatters_by_ft = {
+        blade = { "blade-formatter" }
+    },
+})
+
+require 'nvim-treesitter.configs'.setup { ... }
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.blade = {
+    install_info = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = { "src/parser.c" },
+        branch = "main",
+    },
+    filetype = "blade",
+}
+vim.filetype.add({
+    pattern = {
+        [".*%.blade%.php"] = "blade",
+    },
+})
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,12 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "intelephense",
+      "html-lsp",
+      "typescript-language-server",
+      "prettier",
+      "php-cs-fixer",
+    },
+  },
+}
